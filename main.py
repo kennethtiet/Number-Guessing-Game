f621d9fe065d5db2ab09e08cb74a55b3569d7fd5
KennyT <65835355+tietkenneth@users.noreply.github.com>
2024-09-11 17:04:41 -0700

    Added max_scores

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+max_score = [10,5,3]
+level = -1
 while(True):
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
@@ -8,9 +10,9 @@ while(True):
 
     while(True):
         print("Please select the difficulty level:")
-        print("1. Easy (10 chances)")
-        print("2. Medium (5 chances)")
-        print("3. Hard (3 chances)\n")
+        print("1. Easy (10 chances) Max Score:", 10 - max_score[0])
+        print("2. Medium (5 chances) Max Score:", 5 - max_score[1])
+        print("3. Hard (3 chances) Max Score:", 3 - max_score[2], "\n")
 
         inp = input("Enter a number: ")
         difficulty = "N/A"
@@ -31,7 +33,7 @@ while(True):
         else:
             print("Select a valid number.\n")
 
-
+    level = int(inp)
     print("Great! You have selected the", difficulty,"difficulty level.")
 
     start = time.time()
@@ -41,9 +43,15 @@ while(True):
     while attempts < chances:
         answer = int(input("Enter your guess: "))
         attempts += 1
+        if abs(answer - num) < 5:
+            print("You're close!")
+        
         if answer == num:
             end = time.time()
             length = end - start
+            
+            if attempts < max_score[level-1]:
+                max_score[level-1] = attempts
             
             print("Congratulations! You guessed the correct number in", attempts,"attempts")
             print(f"It took you {length:.3f} seconds")
@@ -52,6 +60,7 @@ while(True):
             print("Incorrect! The number is less than", answer,".")
         else:
             print("Incorrect! The answer is greater than",answer,".")
+            
             
     if attempts >= chances:
         print("Ran out of attempts! The number was", num)
